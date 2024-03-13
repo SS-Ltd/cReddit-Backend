0fffe63884382240616dc4acd46a13e733fb8725
MahmoudSamy1452 <mahmoud.abdelwahed02@eng-st.cu.edu.eg>
2024-03-14 00:29:16 +0200

    Added JWT verification middleware

---
+++ src/middlewares/Verify.js
@@ -0,0 +1,19 @@
+const jwt = require('jsonwebtoken')
+
+function verifyToken (req, res, next) {
+  const token = req.cookies.jwt
+  if (!token) {
+    return res.status(401).json({ message: 'Unauthorized' })
+  }
+
+  try {
+    const decoded = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET)
+    req.decoded = decoded
+    next()
+  } catch (error) {
+    console.error('Error verifying token: ', error)
+    return res.status(403).json({ message: 'Invalid token' })
+  }
+}
+
+module.exports = verifyToken
