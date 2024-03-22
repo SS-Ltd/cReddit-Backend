e8af0c70c7a79c54bee3a79a595d6db2ee10e039
MahmoudSamy1452 <mahmoud.abdelwahed02@eng-st.cu.edu.eg>
2024-03-22 02:57:06 +0200

    Implemented deletePost & restricted media formats on creation

--- src/controllers/Post.js
+++ src/controllers/Post.js
@@ -1,4 +1,5 @@
 const Post = require('../models/Post')
+const mongoose = require('mongoose')
 const cloudinary = require('../utils/Cloudinary')
 const PostUtils = require('../utils/Post')
 
@@ -18,8 +19,8 @@ const createPost = async (req, res) => {
         const dataURI = 'data:' + file.mimetype + ';base64,' + b64
         const { secure_url } = await cloudinary.uploader.upload(dataURI, {
           resource_type: 'auto',
-          folder: 'cReddit'
-          // allowed_formats: ['jpg', 'jpeg', 'png', 'gif', 'mp4', 'svg']
+          folder: 'cReddit',
+          allowed_formats: ['jpg', 'jpeg', 'png', 'gif', 'mp4', 'svg']
         })
         urls.push(secure_url)
       }
@@ -45,6 +46,48 @@ const createPost = async (req, res) => {
   }
 }
 
+const deletePost = async (req, res) => {
+  const postId = req.params.postId
+
+  try {
+    if (!mongoose.Types.ObjectId.isValid(postId)) {
+      return res.status(400).json({ message: 'Invalid post id' })
+    }
+    const post = await Post.findOne({ _id: postId })
+    if (!post) {
+      return res.status(400).json({ message: 'Post is not found' })
+    }
+    if (post.username !== req.decoded.username) {
+      return res.status(403).json({ message: 'You are not authorized to delete this post' })
+    }
+    if (post.type === 'Images & Video') {
+      const publicIDs = post.content.split(' ').map(url => {
+        const matches = url.match(/(cReddit\/.+)\.(.+)/)
+        if (!matches) {
+          return null
+        } else {
+          return matches
+        }
+      })
+      if (publicIDs.includes(null)) {
+        throw new Error('Invalid image or video URLs found in post')
+      }
+      for (const publicID of publicIDs) {
+        if (publicID[2] === 'mp4') {
+          await cloudinary.uploader.destroy(publicID[1], { resource_type: 'video' })
+        } else {
+          await cloudinary.uploader.destroy(publicID[1])
+        }
+      }
+    }
+    await post.deleteOne()
+    res.status(200).json({ message: 'Post deleted successfully' })
+  } catch (error) {
+    res.status(500).json({ message: error.message || 'Error deleting post' })
+  }
+}
+
 module.exports = {
-  createPost
+  createPost,
+  deletePost
 }
