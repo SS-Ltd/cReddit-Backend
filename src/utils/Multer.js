6a641f842271df8e25a4f7f8e6069d4b70ae2d38
Fares Hesham <fares.h.moneim@gmail.com>
2024-04-05 01:27:32 +0200

    Refactor Media.js and Multer.js to handle file uploads and deletions

--- src/utils/Multer.js
+++ src/utils/Multer.js
@@ -3,7 +3,9 @@ const storage = multer.memoryStorage()
 const upload = multer({ storage })
 
 const uploadMultipleImages = upload.array('images', 10)
+const uploadAvatarBanner = upload.fields([{ name: 'avatar', maxCount: 1 }, { name: 'banner', maxCount: 1 }])
 
 module.exports = {
-  uploadMultipleImages
+  uploadMultipleImages,
+  uploadAvatarBanner
 }
