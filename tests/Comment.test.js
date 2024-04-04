3628d7c20f4aacae458e96b412b5becb1387b705
MahmoudSamy1452 <mahmoud.abdelwahed02@eng-st.cu.edu.eg>
2024-04-04 03:09:51 +0200

    Added some tests for editComment

--- tests/Comment.test.js
+++ tests/Comment.test.js
@@ -31,7 +31,7 @@ describe('createComment', () => {
       json: jest.fn()
     }
 
-    PostModel.findOne = jest.fn().mockResolvedValue({ name: 'Test Post', communityName: 'Test Community' })
+    PostModel.findOne = jest.fn().mockResolvedValue({ type: 'Post', name: 'Test Post', communityName: 'Test Community' })
 
     await comment.createComment(req, res)
 
@@ -61,7 +61,7 @@ describe('createComment', () => {
       json: jest.fn()
     }
 
-    PostModel.findOne = jest.fn().mockResolvedValue({ name: 'Test Post', communityName: 'Test Community' })
+    PostModel.findOne = jest.fn().mockResolvedValue({ type: 'Post', name: 'Test Post', communityName: 'Test Community' })
 
     MediaUtils.cloudinary.uploader.upload = jest.fn().mockResolvedValue({ secure_url: 'secure_url' })
 
@@ -94,7 +94,7 @@ describe('createComment', () => {
       json: jest.fn()
     }
 
-    PostModel.findOne = jest.fn().mockResolvedValue({ name: 'Test Post', communityName: 'Test Community' })
+    PostModel.findOne = jest.fn().mockResolvedValue({ type: 'Post', name: 'Test Post', communityName: 'Test Community' })
 
     MediaUtils.cloudinary.uploader.upload = jest.fn().mockResolvedValue({ secure_url: 'secure_url' })
 
@@ -151,7 +151,7 @@ describe('createComment', () => {
     expect(res.json).toHaveBeenCalledWith({ message: 'Comment can only have one image' })
   })
 
-  test('should not create a comment for a non-existing post', async () => {
+  test('should not create a comment for a non-existing post (null)', async () => {
     const req = {
       decoded: { username: 'Test User' },
       body: {
@@ -167,12 +167,207 @@ describe('createComment', () => {
 
     PostModel.findOne = jest.fn().mockResolvedValue(null)
 
-    MediaUtils.cloudinary.uploader.upload = jest.fn().mockResolvedValue({ secure_url: 'secure_url' })
+    await comment.createComment(req, res)
+
+    expect(PostModel).not.toHaveBeenCalled()
+    expect(res.status).toHaveBeenCalledWith(400)
+    expect(res.json).toHaveBeenCalledWith({ message: 'Cannot comment on a non-existing post' })
+  })
+
+  test('should not create a comment for a non-existing post (comment)', async () => {
+    const req = {
+      decoded: { username: 'Test User' },
+      body: {
+        postId: '660d7e17baa5c72965311c7f'
+      },
+      files: [{ buffer: Buffer.from('file1') }]
+    }
+
+    const res = {
+      status: jest.fn().mockReturnThis(),
+      json: jest.fn()
+    }
+
+    PostModel.findOne = jest.fn().mockResolvedValue({ type: 'Comment', name: 'Test Comment', communityName: 'Test Community' })
 
     await comment.createComment(req, res)
 
     expect(PostModel).not.toHaveBeenCalled()
     expect(res.status).toHaveBeenCalledWith(400)
     expect(res.json).toHaveBeenCalledWith({ message: 'Cannot comment on a non-existing post' })
+  })
+})
+
+describe('editComment', () => {
+  beforeEach(() => {
+    jest.clearAllMocks()
+  })
+
+  test('should edit an existing comment with text content when valid commentId and content are provided', async () => {
+    const req = {
+      params: {
+        commentId: '660d7e17baa5c72965311c7f'
+      },
+      body: {
+        content: 'new comment content'
+      },
+      files: []
+    }
+
+    const res = {
+      status: jest.fn().mockReturnThis(),
+      json: jest.fn()
+    }
+
+    PostModel.findOne = jest.fn().mockResolvedValue({
+      _id: 'validCommentId',
+      type: 'Comment',
+      isImage: false,
+      save: jest.fn()
+    })
+    MediaUtils.deleteImages = jest.fn().mockResolvedValue()
+    MediaUtils.uploadImages = jest.fn().mockResolvedValue([])
+
+    await comment.editComment(req, res)
+
+    expect(PostModel.findOne).toHaveBeenCalledWith({ _id: '660d7e17baa5c72965311c7f' })
+    expect(MediaUtils.deleteImages).not.toHaveBeenCalled()
+    expect(MediaUtils.uploadImages).not.toHaveBeenCalled()
+    expect(res.status).toHaveBeenCalledWith(201)
+    expect(res.json).toHaveBeenCalledWith({
+      message: 'Comment edited successfully',
+      commentId: 'validCommentId'
+    })
+  })
+
+  it('should throw an error when attempting to edit a non-existing comment', async () => {
+    const req = {
+      params: {
+        commentId: '660d7e17baa5c72965311c7f'
+      },
+      body: {
+        content: 'new comment content'
+      },
+      files: []
+    }
+    const res = {
+      status: jest.fn().mockReturnThis(),
+      json: jest.fn()
+    }
+
+    PostModel.findOne = jest.fn().mockResolvedValue(null)
+
+    await comment.editComment(req, res)
+
+    expect(PostModel.findOne).toHaveBeenCalledWith({ _id: '660d7e17baa5c72965311c7f' })
+    expect(res.status).toHaveBeenCalledWith(400)
+    expect(res.json).toHaveBeenCalledWith({
+      message: 'Cannot edit a non-existing comment'
+    })
+  })
+
+  test('should edit an existing comment with image content when valid commentId and files are provided', async () => {
+    const req = {
+      params: {
+        commentId: '660d7e17baa5c72965311c7f'
+      },
+      body: {},
+      files: [{ filename: 'image.jpg' }]
+    }
+    const res = {
+      status: jest.fn().mockReturnThis(),
+      json: jest.fn()
+    }
+
+    PostModel.findOne = jest.fn().mockResolvedValue({
+      _id: '660d7e17baa5c72965311c7f',
+      type: 'Comment',
+      isImage: true,
+      content: 'oldImageUrl',
+      save: jest.fn()
+    })
+    MediaUtils.deleteImages = jest.fn().mockResolvedValue()
+    MediaUtils.uploadImages = jest.fn().mockResolvedValue(['newImageUrl'])
+
+    await comment.editComment(req, res)
+
+    expect(PostModel.findOne).toHaveBeenCalledWith({ _id: '660d7e17baa5c72965311c7f' })
+    expect(MediaUtils.deleteImages).toHaveBeenCalledWith(['oldImageUrl'])
+    expect(MediaUtils.uploadImages).toHaveBeenCalledWith([{ filename: 'image.jpg' }])
+    expect(res.status).toHaveBeenCalledWith(201)
+    expect(res.json).toHaveBeenCalledWith({
+      message: 'Comment edited successfully',
+      commentId: '660d7e17baa5c72965311c7f'
+    })
+  })
+
+  test('should not edit an existing comment with image if it has text', async () => {
+    const req = {
+      params: {
+        commentId: '660d7e17baa5c72965311c7f'
+      },
+      body: {},
+      files: [{ filename: 'image.jpg' }]
+    }
+    const res = {
+      status: jest.fn().mockReturnThis(),
+      json: jest.fn()
+    }
+
+    PostModel.findOne = jest.fn().mockResolvedValue({
+      _id: '660d7e17baa5c72965311c7f',
+      type: 'Comment',
+      isImage: false,
+      content: 'text content',
+      save: jest.fn()
+    })
+    MediaUtils.deleteImages = jest.fn().mockResolvedValue()
+    MediaUtils.uploadImages = jest.fn().mockResolvedValue(['newImageUrl'])
+
+    await comment.editComment(req, res)
+
+    expect(PostModel.findOne).toHaveBeenCalledWith({ _id: '660d7e17baa5c72965311c7f' })
+    expect(MediaUtils.deleteImages).not.toHaveBeenCalledWith(['oldImageUrl'])
+    expect(MediaUtils.uploadImages).not.toHaveBeenCalledWith([{ filename: 'image.jpg' }])
+    expect(res.status).toHaveBeenCalledWith(400)
+    expect(res.json).toHaveBeenCalledWith({
+      message: 'No text provided'
+    })
+  })
+
+  test('should not edit an existing comment with text if it has image', async () => {
+    const req = {
+      params: {
+        commentId: '660d7e17baa5c72965311c7f'
+      },
+      body: {
+        content: 'new comment content'
+      },
+      files: []
+    }
+    const res = {
+      status: jest.fn().mockReturnThis(),
+      json: jest.fn()
+    }
+
+    PostModel.findOne = jest.fn().mockResolvedValue({
+      _id: '660d7e17baa5c72965311c7f',
+      type: 'Comment',
+      isImage: true,
+      content: 'oldImageUrl',
+      save: jest.fn()
+    })
+    MediaUtils.deleteImages = jest.fn().mockResolvedValue()
+    MediaUtils.uploadImages = jest.fn().mockResolvedValue(['newImageUrl'])
+
+    await comment.editComment(req, res)
+
+    expect(PostModel.findOne).toHaveBeenCalledWith({ _id: '660d7e17baa5c72965311c7f' })
+    expect(MediaUtils.deleteImages).not.toHaveBeenCalledWith(['oldImageUrl'])
+    expect(MediaUtils.uploadImages).not.toHaveBeenCalledWith([{ filename: 'image.jpg' }])
+    expect(res.status).toHaveBeenCalledWith(400)
+    expect(res.json).toHaveBeenCalledWith({
+      message: 'No image provided'
+    })
   })
 })
