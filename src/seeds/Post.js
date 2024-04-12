a0dcaa5daf965fb2b08a95d0b75d0a5a915fb537
Fares Hesham <fares.h.moneim@gmail.com>
2024-04-12 19:21:46 +0200

    Fixed Seeding of Comments and Posts
    Co-authored-by: Mahmoud Samy <MahmoudSamy1452@users.noreply.github.com>

--- src/seeds/Post.js
+++ src/seeds/Post.js
@@ -1,18 +1,18 @@
 const { faker } = require('@faker-js/faker')
 const PostModel = require('../models/Post')
-const { postIDs, usernames, communityNames } = require('./SeedUtils')
+const { posts, usernames } = require('./SeedUtils')
 
-const posts = []
+const postsSeed = []
 
 function createRandomPosts () {
-  for (let i = 0; i < postIDs.length / 2; i++) {
-    const upvotes = faker.number.int({ min: 10000, max: 100000 })
-    const downvotes = faker.number.int({ min: 0, max: 50000 })
+  for (let i = 0; i < posts.length / 2; i++) {
+    const upvotes = faker.number.int({ min: 1000, max: 10000 })
+    const downvotes = faker.number.int({ min: 0, max: 5000 })
 
-    posts.push({
-      _id: postIDs[i],
+    postsSeed.push({
+      _id: posts[i].postId,
       username: faker.helpers.arrayElement(usernames),
-      communityName: faker.helpers.arrayElement(communityNames),
+      communityName: posts[i].communityId,
       title: faker.lorem.sentence(),
       content: faker.lorem.paragraph(),
       pollOptions: [],
@@ -37,29 +37,27 @@ function createRandomPosts () {
 }
 
 function createRandomImages () {
-  for (let i = postIDs.length / 2; i < postIDs.length * 3 / 4; i++) {
+  for (let i = posts.length / 2; i < posts.length * 3 / 4; i++) {
     const upvotes = faker.number.int({ min: 10000, max: 100000 })
     const downvotes = faker.number.int({ min: 0, max: 50000 })
     const Images = [
-      'https://res.cloudinary.com/dfvgbxwed/video/upload/v1711805795/cReddit/exvhyjzxctf543rgpcii.mp4',
-      'https://res.cloudinary.com/dfvgbxwed/video/upload/v1711817782/cReddit/cxcohmaylhmoam1o6fgb.mp4',
-      'https://res.cloudinary.com/dfvgbxwed/video/upload/v1711817782/cReddit/lpuqv3dsqdpxrao9knie.mp4',
-      'https://res.cloudinary.com/dfvgbxwed/video/upload/v1711817859/cReddit/kiwbhvfcwqbyalxolbw5.mp4',
-      'https://res.cloudinary.com/dfvgbxwed/video/upload/v1711818207/cReddit/cifgf1a8wwikpqb5ahxg.mp4',
-      'https://res.cloudinary.com/dfvgbxwed/video/upload/v1711831051/cReddit/jeheha6t84u0k4belhsm.mp4',
-      'https://res.cloudinary.com/dfvgbxwed/video/upload/v1711831661/cReddit/vrtnnp14r4el9tcap4fz.mp4',
-      'https://res.cloudinary.com/dfvgbxwed/video/upload/v1712160280/cReddit/xjlbzyjy0nf5tb1ykgm5.mp4',
-      'https://res.cloudinary.com/dfvgbxwed/video/upload/v1712187796/cReddit/umza5msxgjrqsijqt0kn.mp4',
-      'https://res.cloudinary.com/dfvgbxwed/video/upload/v1712187823/cReddit/ri4hh96pzeo99vcv5kjt.mp4',
-      'https://res.cloudinary.com/dfvgbxwed/image/upload/v1712189603/cReddit/vtlfqumkvzzhcecfx1qb.png',
-      'https://res.cloudinary.com/dfvgbxwed/video/upload/v1712189944/cReddit/ggia1spudhvuwqulvxto.mp4',
-      'https://res.cloudinary.com/dfvgbxwed/image/upload/v1712201118/cReddit/vibm93ekhji0qpmjnzvl.png'
+      'https://res.cloudinary.com/dfvgbxwed/image/upload/v1712940605/cReddit/mctqrblgxlp9kxgdg3bu.jpg',
+      'https://res.cloudinary.com/dfvgbxwed/image/upload/v1712940606/cReddit/gmt1fjuglf6qfg1xgk4l.jpg',
+      'https://res.cloudinary.com/dfvgbxwed/image/upload/v1712940607/cReddit/nyphtgmexxqecnloktoa.jpg',
+      'https://res.cloudinary.com/dfvgbxwed/image/upload/v1712940608/cReddit/uo2aiw0lpff0vdzfexkl.jpg',
+      'https://res.cloudinary.com/dfvgbxwed/image/upload/v1712940608/cReddit/dzyfdsqhbnv4cec49cgg.jpg',
+      'https://res.cloudinary.com/dfvgbxwed/image/upload/v1712940609/cReddit/oaoyfotrjw50vulncbam.jpg',
+      'https://res.cloudinary.com/dfvgbxwed/image/upload/v1712940610/cReddit/ccyhctzoctdadrpvuqtl.jpg',
+      'https://res.cloudinary.com/dfvgbxwed/image/upload/v1712940632/cReddit/rjeou0ztju9hkcmrhiyr.png',
+      'https://res.cloudinary.com/dfvgbxwed/image/upload/v1712940633/cReddit/pgs2hbjrahrlg31ln4hb.png',
+      'https://res.cloudinary.com/dfvgbxwed/image/upload/v1712940635/cReddit/qgz2kaegvyvpvfargmry.png',
+      'https://res.cloudinary.com/dfvgbxwed/image/upload/v1712940646/cReddit/lscnoowxpzp0bvplfuph.gif',
+      'https://res.cloudinary.com/dfvgbxwed/image/upload/v1712940647/cReddit/yrlsa07bjpmewrvl2gpx.gif'
     ]
-
-    posts.push({
-      _id: postIDs[i],
+    postsSeed.push({
+      _id: posts[i].postId,
       username: faker.helpers.arrayElement(usernames),
-      communityName: faker.helpers.arrayElement(communityNames),
+      communityName: posts[i].communityId,
       title: faker.lorem.sentence(),
       content: faker.helpers.arrayElement(Images),
       pollOptions: [],
@@ -84,7 +82,7 @@ function createRandomImages () {
 }
 
 function createRandomPolls () {
-  for (let i = postIDs.length * 3 / 4; i < postIDs.length * 7 / 8; i++) {
+  for (let i = posts.length * 3 / 4; i < posts.length * 7 / 8; i++) {
     const upvotes = faker.number.int({ min: 10000, max: 100000 })
     const downvotes = faker.number.int({ min: 0, max: 50000 })
 
@@ -113,10 +111,10 @@ function createRandomPolls () {
       potentialVoters = potentialVoters.filter(user => !selectedUsers.includes(user))
     }
 
-    posts.push({
-      _id: postIDs[i],
+    postsSeed.push({
+      _id: posts[i].postId,
       username: faker.helpers.arrayElement(usernames),
-      communityName: faker.helpers.arrayElement(communityNames),
+      communityName: posts[i].communityId,
       title: faker.lorem.sentence(),
       content: faker.lorem.paragraph(),
       pollOptions: pollOptions.slice(0, faker.number.int({ min: 2, max: 5 })),
@@ -141,7 +139,7 @@ function createRandomPolls () {
 }
 
 function createRandomLinks () {
-  for (let i = postIDs.length * 7 / 8; i < postIDs.length; i++) {
+  for (let i = posts.length * 7 / 8; i < posts.length; i++) {
     const upvotes = faker.number.int({ min: 10000, max: 100000 })
     const downvotes = faker.number.int({ min: 0, max: 50000 })
     const links = [
@@ -160,10 +158,10 @@ function createRandomLinks () {
       'https://res.cloudinary.com/dfvgbxwed/image/upload/v1712201118/cReddit/vibm93ekhji0qpmjnzvl.png'
     ]
 
-    posts.push({
-      _id: postIDs[i],
+    postsSeed.push({
+      _id: posts[i].postId,
       username: faker.helpers.arrayElement(usernames),
-      communityName: faker.helpers.arrayElement(communityNames),
+      communityName: posts[i].communityId,
       title: faker.lorem.sentence(),
       content: faker.helpers.arrayElement(links),
       pollOptions: [],
@@ -193,7 +191,7 @@ async function seedPosts () {
   createRandomPolls()
   createRandomLinks()
   await PostModel.deleteMany({})
-  await PostModel.insertMany(posts)
+  await PostModel.insertMany(postsSeed)
   console.log('Posts seeded')
 }
 
