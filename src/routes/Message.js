9d3df2fc199bac0710efa6c1c634c0bae41a6ba2
Fares Hesham <fares.h.moneim@gmail.com>
2024-04-28 20:39:58 +0300

    Fix typo in route definition in Message.js

--- src/routes/Message.js
+++ src/routes/Message.js
@@ -1,12 +1,12 @@
 const express = require('express')
-const router = express.router
+const router = express.Router()
 
 const message = require('../controllers/Message')
 const { verifyToken } = require('../middlewares/Verify')
 
-router.rout('/').post(verifyToken, message.createMessage).get(verifyToken, message.getMessages)
-router.rout('/sent').get(verifyToken, message.getSentMessages)
-router.rout('/unread').get(verifyToken, message.getUnreadMessages)
+router.route('/').post(verifyToken, message.createMessage).get(verifyToken, message.getMessages)
+router.route('/sent').get(verifyToken, message.getSentMessages)
+router.route('/unread').get(verifyToken, message.getUnreadMessages)
 router.route('/:messageId/mark-as-read').patch(verifyToken, message.markAsRead)
 router.route('/mark-all-as-read').put(verifyToken, message.markAllAsRead)
 router.route('/:messageId').delete(verifyToken, message.deleteMessage)
