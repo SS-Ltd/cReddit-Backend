be9defff4df4309394d70b0f3f0a44132c850b8b
abdallahahmed10 <abdallah.ahmed581@gmail.com>
2024-05-01 00:36:33 +0300

    Adding event listeners for chatMessage, joinRoom, leaveRoom

--- src/utils/Socket.js
+++ src/utils/Socket.js
@@ -1,21 +1,29 @@
-
 const connectSocket = (io) => {
-    console.log('Connecting to the server: ', io)
-    return io.on('connection', (socket) => {
-        socket.join('room')
-        console.log('Connected to the server')
+  console.log('Connecting to the server: ', io)
+  return io.on('connection', (socket) => {
+    socket.join('room')
+    console.log('Connected to the server')
 
-        socket.on('disconnect', () => {
-            console.log('Disconnected from the server')
-        })
+    socket.on('disconnect', () => {
+      console.log('Disconnected from the server')
     })
+
+    socket.on('chatMessage', (msg) => {
+      console.log('Message: ', msg)
+    })
+
+    socket.on('joinRoom', (room) => {
+      socket.join(room)
+      console.log('Joined room: ', room)
+    })
+
+    socket.on('leaveRoom', (room) => {
+      socket.leave(room)
+      console.log('Left room: ', room)
+    })
+  })
 }
 
-const emitSocketEvent = (req, roomId, event, payload) => {
-    req.app.get("io").in(roomId).emit(event, payload);
-  };
-
 module.exports = {
-    connectSocket,
-    emitSocketEvent
+  connectSocket
 }
