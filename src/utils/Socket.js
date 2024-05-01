bf9616b341fbc7f24689e8cdad97ccaa11e35cd0
abdallahahmed10 <abdallah.ahmed581@gmail.com>
2024-05-01 21:06:43 +0300

    Modified chatMessage and joinRoom to include validations

--- src/utils/Socket.js
+++ src/utils/Socket.js
@@ -1,19 +1,59 @@
+const ChatMessageModel = require('../models/ChatMessage')
+const ChatRoomModel = require('../models/ChatRoom')
+const UserModel = require('../models/User')
+
 const connectSocket = (io) => {
   console.log('Connecting to the server: ', io)
   return io.on('connection', (socket) => {
-    socket.join(socket.roomId)
     console.log('Connected to the server')
 
     socket.on('disconnect', () => {
       console.log('Disconnected from the server')
     })
 
-    socket.on('chatMessage', (msg) => {
-      console.log('Message: ', msg)
+    socket.on('chatMessage', async (data) => { // data = { roomId, message }
+      console.log('Message: ', data)
+      const { username, roomId, message } = data
+      const chatRoom = await ChatRoomModel.findById({ _id: roomId, isDeleted: false })
+      if (!chatRoom) {
+        return socket.emit('error', { message: 'Chat room not found' })
+      }
+
+      const user = await UserModel.findOne({ username, isDeleted: false })
+      if (!user) {
+        return socket.emit('error', { message: 'User not found' })
+      }
+
+      if (!chatRoom.members.includes(username) && chatRoom.host !== username) {
+        return socket.emit('error', { message: 'User is not a member of this chat room' })
+      }
+
+      socket.to(data.roomId).emit('newMessage', data.message)
+      const chatMessage = new ChatMessageModel({
+        user: username,
+        content: message,
+        room: roomId
+      })
+      await chatMessage.save()
     })
 
-    socket.on('joinRoom', (data) => {
+    socket.on('joinRoom', async (data) => {
       const { username, room } = data
+
+      const user = await UserModel.findOne({ username, isDeleted: false })
+      if (!user) {
+        return socket.emit('error', { message: 'User not found' })
+      }
+
+      const chatRoom = await ChatRoomModel.findById({ _id: room, isDeleted: false })
+      if (!chatRoom) {
+        return socket.emit('error', { message: 'Chat room not found' })
+      }
+
+      if (!chatRoom.members.includes(username)) {
+        return socket.emit('error', { message: 'User is not a member of this chat room' })
+      }
+
       socket.join(room)
       console.log('Joined room: ', room)
       socket.to(room).emit('newUser', { username, room })
