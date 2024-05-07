1c30abcb6e58c7d40f9051cf24334de8818e0030
Fares Hesham <fares.h.moneim@gmail.com>
2024-05-07 20:06:44 +0300

    😎 Added get communities where the user is moderator in

--- src/controllers/User.js
+++ src/controllers/User.js
@@ -1286,6 +1286,29 @@ const getUserOverview = async (req, res) => {
   }
 }
 
+const getModeratorIn = async (req, res) => {
+  try {
+    const username = req.decoded.username
+
+    const user = await UserModel.findOne({ username: username, isDeleted: false })
+    const communities = user.moderatorInCommunities
+
+    const moderatorIn = []
+    for (let i = 0; i < communities.length; i++) {
+      const community = await CommunityModel.findOne({ name: communities[i] })
+      moderatorIn.push({
+        name: community.name,
+        icon: community.icon,
+        members: community.members
+      })
+    }
+
+    res.status(200).json(moderatorIn)
+  } catch (error) {
+    res.status(500).json({ message: 'Error getting communities where the user is moderator' })
+  }
+}
+
 module.exports = {
   getUser,
   follow,
@@ -1311,5 +1334,6 @@ module.exports = {
   getHistory,
   clearHistory,
   getJoinedCommunities,
-  getUserOverview
+  getUserOverview,
+  getModeratorIn
 }
