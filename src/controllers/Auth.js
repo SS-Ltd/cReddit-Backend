68ca3f49517b3e202e057619e5aa8b15f25bd608
Moaaz Tarek <64367134+moa234@users.noreply.github.com>
2024-05-15 13:16:23 +0300

    Update Auth.js

--- src/controllers/Auth.js
+++ src/controllers/Auth.js
@@ -2,8 +2,6 @@ const User = require('../models/User')
 const bcrypt = require('bcrypt')
 const emailValidator = require('email-validator')
 const { faker } = require('@faker-js/faker')
-const { sendVerificationEmail } = require('../utils/Email')
-const { generateTokens, decryptToken } = require('./JWT')
 
 const validatePassword = (password) => {
   const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[\w!@#$%^&*()\-_=+\\|[\]{};:'",.<>/?`~ ])[\w!@#$%^&*()\-_=+\\|[\]{};:'",.<>/?`~ ]{8,}$/
